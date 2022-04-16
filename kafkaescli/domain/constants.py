6f843ad46dbd1f13db84bf3da57846ff4654bd7b
Jony Kalavera <mr.jony@gmail.com>
2022-04-16 19:38:04 +0200

    fix app version constant

--- kafkaescli/domain/constants.py
+++ kafkaescli/domain/constants.py
@@ -8,7 +8,7 @@ APP_PACKAGE = "kafkaescli"
 try:
     APP_VERSION = pkg_resources.get_distribution(APP_PACKAGE).version
 except pkg_resources.DistributionNotFound:
-    APP_VERSION = version(APP_PACKAGE)
+    APP_VERSION = 'development'
 
 DEFAULT_BOOTSTRAP_SERVERS = "localhost:9092"
 DEFAULT_CONFIG_FILE_PATH = "~/.kafkaescli/config.json"
